e50b5adfa7e21c5f79226b671993c7acc0b51bae
Pedro Paulo <0901dantaspedro@gmail.com>
2023-06-12 22:54:44 -0300

    Melhorando o método adiciona - OPÇÃO 2

--- src/Vetor/Vetor.java
+++ src/Vetor/Vetor.java
@@ -10,13 +10,12 @@ public class Vetor {
         this.tamanho = 0;
     }
 
-    public void adiciona(String elemento) throws Exception{
-
-        if(this.tamanho < this.elementos.length) {
+    public boolean adiciona(String elemento){
+        if(this.tamanho < this.elementos.length){
             this.elementos[this.tamanho] = elemento;
             this.tamanho++;
-        }else{
-            throw new Exception("Vetor ja esta cheio, nao eh possivel adicionar mais elementos");
+            return true;
         }
+        return false;
     }
 }

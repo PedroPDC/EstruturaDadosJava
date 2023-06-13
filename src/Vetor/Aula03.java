30a951af50f1375456135b8b071ee83584fa5071
Pedro Paulo <0901dantaspedro@gmail.com>
2023-06-12 22:52:00 -0300

    Melhorando o método adiciona - OPÇÃO 1

--- src/Vetor/Aula03.java
+++ src/Vetor/Aula03.java
@@ -3,9 +3,14 @@ package Vetor;
 public class Aula03 {
     public static void main(String[] args) {
 
-        Vetor vetor = new Vetor(5);
+        Vetor vetor = new Vetor(2);
 
-        vetor.adiciona("elemento 1");
-        vetor.adiciona("elemento 2");
+        try {
+            vetor.adiciona("elemento 1");
+            vetor.adiciona("elemento 2");
+            vetor.adiciona("elemento 3");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }

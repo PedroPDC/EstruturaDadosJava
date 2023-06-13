bd0ff84e501e6ebf276fb497c1bcd302a1713946
Pedro Paulo <0901dantaspedro@gmail.com>
2023-06-12 23:38:14 -0300

    Metodo para adicionar elemento em qualquer posição

--- src/Vetor/Vetor.java
+++ src/Vetor/Vetor.java
@@ -21,6 +21,24 @@ public class Vetor {
         return false;
     }
 
+    // 0 1 2 3 4 5 6 = tamanho é 5
+    // B C E F G + +
+    public boolean adiciona(int posicao, String elemento){
+        //verifica se posição é valida ou nao
+        if(!(posicao >= 0 && posicao < tamanho)){
+            throw new IllegalArgumentException("Posição inválida!");
+        }
+        //mover todos os elementos
+        for(int i = this.tamanho-1; i >= posicao; i--){
+            this.elementos[i+1] = this.elementos[i];
+        }
+        //adicionar um elemento
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+
+        return true;
+    }
+
     public String busca(int posicao){
         //range de posições que nao podem ser acessadas devido ao !
         if(!(posicao >= 0 && posicao < tamanho)) {

9506e58d1d62e572ab89aaf1bafd7eb959d8592f
Pedro Paulo <0901dantaspedro@gmail.com>
2023-06-12 22:40:03 -0300

    Classes vetor e Aula02 criadas

---
+++ src/Vetor/Aula02.java
@@ -0,0 +1,8 @@
+package Vetor;
+
+public class Aula02 {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(5);
+    }
+}
---
+++ src/Vetor/Vetor.java
@@ -0,0 +1,10 @@
+package Vetor;
+
+public class Vetor {
+
+    private String[] elementos;
+
+    public Vetor(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+}
